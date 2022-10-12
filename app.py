72304ea2b66b0d42a59fb554e00cf37465271397
martin fonseca <martinfonseca704@gmail.com>
2022-10-12 02:41:32 -0400

    3rd push

--- app.py
+++ app.py
@@ -1,13 +1,100 @@
-#Create a New Python File and Import the Flask Dependency // will enable your code to access all that Flask has to offer.
-from flask import Flask
-#Create a New Flask App Instance // ready to create a new Flask app instance. "Instance" is a general term in programming to 
-# refer to a singular version of something. Add the following to your code to create a new Flask instance called app:
+# Module 9 //  Import the Flask Dependency
+#################################################
+
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+# Module 9.5.1 // Set Up the Database and Flask
+#################################################
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+# Create a New Flask App Instance
+#################################################
 app = Flask(__name__)
-#Create Flask Routes // define the starting point, also known as the root.
+
+# Module 9.5.2 // Create the Welcome Route
+#################################################
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
-#Run a Flask App // To run the app, we need to use the command line to navigate to the folder where we've saved our code.
-# You should save this code in the same folder you've used in the rest of this module.
-#For our FLASK_APP environment variable, we want to modify the path that will run our app.py file so that we can run our file.
-#export FLASK_APP=app.py
+def welcome():
+    return(
+        f"Welcome to the Hawaii Climate Analysis API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+    )
+
+# Module 9.5.3 // Precipitation Route
+#################################################
+@app.route("/api/v1.0/precipitation")
+
+def precipitation():
+   prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+   precipitation = session.query(Measurement.date, Measurement.prcp).\
+    filter(Measurement.date >= prev_year).all()
+   precip = {date: prcp for date, prcp in precipitation}
+   return jsonify(precip)
+
+# 9.5.4 // Stations Route
+#################################################
+@app.route("/api/v1.0/stations")
+
+def stations():
+    results = session.query(Station.station).all()
+    stations = list(np.ravel(results))
+    return jsonify(stations=stations)
+
+# 9.5.5 // Monthly Temperature Route
+#################################################
+@app.route("/api/v1.0/tobs")
+
+def temp_monthly():
+    prev_year = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+    results = session.query(Measurement.tobs).\
+      filter(Measurement.station == 'USC00519281').\
+      filter(Measurement.date >= prev_year).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps=temps)
+
+# 9.5.6 // Statistics Route
+#################################################
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        filter(Measurement.date <= end).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
